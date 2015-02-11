122316a8d45c87fc18fea2ed835b1e749900aa15
rtuita23 <rktuita23@hotmail.com>
2015-02-11 07:24:47 +0000

    jedi view

---
+++ hello_world.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/hello/<name>')
+def hello_person(name):
+    html = '''
+        <h1>
+            Hello {}!
+        </h1>
+        <p>
+            Here's a picture of kitten. Awww...
+        </p>
+        <img src='http://placekitten.com/g/200/300'>
+    '''
+    return html.format(name.title())
+
+@app.route('/jedi/<fname>/<lname>')
+def jedi_name(fname, lname):
+    lname_3 = lname[0:3]
+    fname_2 = fname[0:2]
+    jedi = lname_3 + fname_2
+    
+    html = '''
+        <h1>
+             Hello {} {}!
+        </h1>
+        <p>
+            Your Jedi name is {}!
+        </p>
+    '''
+    return html.format(fname.title(), lname.title(), jedi.title())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+    
+    
