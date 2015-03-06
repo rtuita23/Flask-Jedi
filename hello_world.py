d03140b5336920b1e11bfff4b96c0f4d9f531127
rtuita23 <rktuita23@hotmail.com>
2015-03-06 08:19:22 +0000

    remove hello view

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/')
 def hello_world():
     return 'Hello World'
 
@@ -36,6 +36,4 @@ def jedi_name(fname, lname):
     return html.format(fname.title(), lname.title(), jedi.title())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-    
-    
+    app.run(host='0.0.0.0', port=8888)
